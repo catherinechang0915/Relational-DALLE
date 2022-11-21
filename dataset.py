2407cdee528fd9df3812c14977b4302ccf5123c8
Ruimeng Chang <catherine.chang0915@gmail.com>
2022-11-21 13:41:27 -0800

    fix dataset for one hot label

--- dataset.py
+++ dataset.py
@@ -23,5 +23,5 @@ class SortOfClevrDataset(Dataset):
     def __getitem__(self, idx):
         image = self.images[idx // 15]
         text = self.questions[idx]
-        y = self.answers[idx]
+        y = self.answers[idx][0]
         return torch.tensor(image).float(), torch.tensor(text).squeeze(0), torch.tensor(y)
